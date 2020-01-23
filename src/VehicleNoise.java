22268c3875f4895a14051e6b704fee75c2465718
kjkleindorfer <kjkleindorfer@dmacc.edu>
2020-01-23 17:38:13 -0600

    Organizing files
    Somehow, all the files were put inside a src/model/src model folder.... I believe from Josh's pull request.  The project has been refactored to remove the redundancy.

--- src/VehicleNoise.java
+++ src/VehicleNoise.java
@@ -11,11 +11,14 @@ public class VehicleNoise {
 		Boat baglietto = new Boat();
 		System.out.println(baglietto.makeNoise());
     
-    		Camper jayco = new Camper();
+    	Camper jayco = new Camper();
 		System.out.println(jayco.makeNoise());
 		
 		Bus blueBird = new Bus();
 		System.out.println(blueBird.makeNoise());
+		
+		Truck someToyota = new Truck("Tacoma", 2016, "4WD");
+		System.out.println(someToyota.makeNoise());
 	}
 
 }
